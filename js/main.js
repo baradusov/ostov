4335adeafb1be39873b85c12b36609975939fd11
baradusov <baradusovnh@gmail.com>
2018-02-11 19:01:25 +0400

    init commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    fade: true,
+    speed: 1000,
+    waitForAnimate: false,
+    prevArrow: '<button type="button" class="slick-prev"><svg width="60px" height="80px" viewBox="0 0 50 80" xml:space="preserve"><polyline fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="60px" height="80px" viewBox="0 0 50 80" xml:space="preserve"><polyline fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/></svg></button>',
+});
+
+  $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    $('.slick-active').find('.slide-title').addClass('fadeOutRight');
+  });
+});
